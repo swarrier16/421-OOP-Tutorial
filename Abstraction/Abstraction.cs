c70e67f38e097646c6e3833d7494be34ba72d4ab
swarrier16 <swarrier16@gmail.com>
2020-02-21 07:10:37 -0800

    updated abstraction

--- Abstraction/Abstraction.cs
+++ Abstraction/Abstraction.cs
@@ -4,6 +4,35 @@ namespace Abstraction
 {
     public class Abstracting
     {
+        abstract class Majors //abstract class
+        {
+            public abstract int GetGPA(); //abstract method
+        }
 
+        class CS : Majors
+        {
+            private int grade;
+            public CS(int g)
+            {
+                grade = g;
+            }
+            public override int GetGPA()
+            {
+                return grade;
+            }
+        }
+
+        class English : Majors
+        {
+            private int grade;
+            public English(int g)
+            {
+                grade = g;
+            }
+            public override int GetGPA()
+            {
+                return grade;
+            }
+        }
     }
 }
